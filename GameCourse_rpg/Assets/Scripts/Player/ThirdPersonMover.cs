386131881de1a9d79a700535fb9b2ba3a90bd159
ComradeCrunchy <49103834+ComradeCrunchy@users.noreply.github.com>
2021-05-11 14:14:06 -0400

    Animation blend tree

--- GameCourse_rpg/Assets/Scripts/Player/ThirdPersonMover.cs
+++ GameCourse_rpg/Assets/Scripts/Player/ThirdPersonMover.cs
@@ -8,25 +8,36 @@ public class ThirdPersonMover : MonoBehaviour
     [SerializeField] private float _turnspeed = 1000f;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Rigidbody rb;
+    private Animator _anim;
+
+    private void Start()
+    {
+      if (rb == null) 
+      {
+          rb = GetComponent<Rigidbody>();
+      }
+
+      _anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         var mouseMovement = Input.GetAxis("Mouse X");
         transform.Rotate(0, mouseMovement* Time.deltaTime * _turnspeed, 0);
-        if (rb == null)
-        {
-            rb = GetComponent<Rigidbody>();
-        }
+        
     }
 
     private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift))
+            vertical *= 2f;
 
         var velocity = new Vector3(horizontal, 0, vertical);
         velocity *= moveSpeed * Time.fixedDeltaTime;
         Vector3 offset = transform.rotation * velocity;
         rb.MovePosition(transform.position + offset);
+        _anim.SetFloat("Speed", vertical, 0.1f, Time.deltaTime);
     }
 }
