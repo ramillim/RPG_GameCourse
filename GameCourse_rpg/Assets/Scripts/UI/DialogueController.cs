e5ec3c4c2e8cd8d62d009ce14dc1f87bc45871c3
ComradeCrunchy <49103834+ComradeCrunchy@users.noreply.github.com>
2021-05-12 13:22:04 -0400

    Dialogue Controller

--- GameCourse_rpg/Assets/Scripts/UI/DialogueController.cs
+++ GameCourse_rpg/Assets/Scripts/UI/DialogueController.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
 
 public class DialogueController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextAsset _dialogue;
+    [SerializeField] private TMP_Text _storyText;
+    [SerializeField] private Button[] _choiceButtons;
+    Story _story;
+    [ContextMenu("Start Dialogue")]
+    private void StartDialogue()
     {
-        
+        _story = new Story(_dialogue.text);
+        RefreshView();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RefreshView()
     {
-        
+        StringBuilder storyTextBuilder = new StringBuilder();
+        while (_story.canContinue)
+            storyTextBuilder.AppendLine(_story.Continue());
+        _storyText.SetText(storyTextBuilder);
+        for (int i = 0; i < _choiceButtons.Length; i++)
+        {
+            var button = _choiceButtons[i];
+            button.gameObject.SetActive(i < _story.currentChoices.Count);
+            button.onClick.RemoveAllListeners();
+            if (i < _story.currentChoices.Count)
+            {
+                var choice = _story.currentChoices[i];
+                button.GetComponentInChildren<TMP_Text>().SetText(choice.text);
+                button.onClick.AddListener(() =>
+                {
+                    _story.ChooseChoiceIndex(choice.index);
+                    RefreshView();
+                });
+            }
+        }
     }
 }
