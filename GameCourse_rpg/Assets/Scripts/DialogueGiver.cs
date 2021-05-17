f801c1c09265476efd9689411e3b12506bf18575
ComradeCrunchy <49103834+ComradeCrunchy@users.noreply.github.com>
2021-05-17 15:28:25 -0400

    more dialogue integration

--- GameCourse_rpg/Assets/Scripts/DialogueGiver.cs
+++ GameCourse_rpg/Assets/Scripts/DialogueGiver.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueGiver : MonoBehaviour
 {
@@ -13,14 +14,34 @@ public class DialogueGiver : MonoBehaviour
 
     public GiverType giver;
     [SerializeField] private TextAsset _dialogue;
+    [SerializeField] private GameObject _panel;
+    private bool _isTalking;
+
+    private void Update()
+    {
+        if (_isTalking)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                FindObjectOfType<DialogueController>().StartDialogue(_dialogue);
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         var player = other.GetComponent<ThirdPersonMover>();
         if (player != null)
         {
-            FindObjectOfType<DialogueController>().StartDialogue(_dialogue);
+            _panel.SetActive(true);
+            _isTalking = true;
             if(giver == GiverType.Npc)
                 transform.LookAt(player.transform);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _panel.SetActive(false);
     }
 }
