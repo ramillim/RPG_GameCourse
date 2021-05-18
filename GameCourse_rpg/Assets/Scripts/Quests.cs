a28a35af53be125895ded1844313393190a14021
ComradeCrunchy <49103834+ComradeCrunchy@users.noreply.github.com>
2021-05-18 18:14:01 -0400

    Started quest Structure

---
+++ GameCourse_rpg/Assets/Scripts/Quests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Quest")]
+public class Quests : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField] private string _discription;
+
+    [Tooltip("Designer Notes, not visable to player.")] [SerializeField]
+    private string _notes;
+    
+    public List<Step> Steps;
+}
+[Serializable]
+public class Step
+{
+    [SerializeField] private string _instructions;
+    public List<Objectives> Objectives;
+}
+[Serializable]
+public class Objectives
+{
+    [SerializeField] private ObjectiveType _objectivetype;
+    public enum ObjectiveType
+    {
+        Flag,
+        Item,
+        Kill,
+    }
+}
