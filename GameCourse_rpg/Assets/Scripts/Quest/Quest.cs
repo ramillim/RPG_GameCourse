47be4f86afaebe2e34a505a9d95526165dee3daa
ComradeCrunchy <49103834+ComradeCrunchy@users.noreply.github.com>
2021-05-31 14:10:44 -0400

    its fine.....

--- GameCourse_rpg/Assets/Scripts/Quest/Quest.cs
+++ GameCourse_rpg/Assets/Scripts/Quest/Quest.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [CreateAssetMenu(menuName = "Quest")]
 public class Quest : ScriptableObject
