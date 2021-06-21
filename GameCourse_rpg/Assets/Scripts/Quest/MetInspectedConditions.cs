743a75f2ca0cc330c31633633fbde15ea67cab82
ComradeCrunchy <49103834+ComradeCrunchy@users.noreply.github.com>
2021-06-20 21:49:51 -0400

    last sunday push

--- GameCourse_rpg/Assets/Scripts/Quest/MetInspectedConditions.cs
+++ GameCourse_rpg/Assets/Scripts/Quest/MetInspectedConditions.cs
@@ -1,7 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class MetInspectedConditions : MonoBehaviour, IMet
 {
     [SerializeField] Inspectable _requiredInspectable;
     public bool Met() => _requiredInspectable.WasFullyInspected;
+
+    void OnDrawGizmos()
+    {
+
+        if (_requiredInspectable != null)
+        {
+            Gizmos.color = Color.green;
+            //Gizmos.color = Met() ? Color.green : Color.red;
+            Gizmos.DrawLine(transform.position, _requiredInspectable.transform.position);
+        }
+            
+    }
 }
